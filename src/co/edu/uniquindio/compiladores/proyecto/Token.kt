1d76962562cbe47bb1e00036c7e6765ceea43f6e
Danielafpolaania <92338657+Danielafpolaania@users.noreply.github.com>
2021-10-15 15:03:45 -0500

    Update Token.kt

--- src/co/edu/uniquindio/compiladores/proyecto/Token.kt
+++ src/co/edu/uniquindio/compiladores/proyecto/Token.kt
@@ -1,5 +1,13 @@
 package co.edu.uniquindio.compiladores.proyecto
 
+
+/**
+ * Clase que representa un token o una categoria lexica del lenguaje
+ * @author Laura Daniela Florez Polania
+ *  @author Alex Ferney Vargas
+ *   @author Andres Felipe Betancourt
+ *
+ */
 class Token (var palabra:String, var categoria:Categoria, var fila:Int, var columna:Int ) {
 
 
