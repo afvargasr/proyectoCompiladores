9f35fbaaa1e85d548b77842e79c86a6ad80fbc1d
andrewbg24 <afbetancourtg@uqvirtual.edu.co>
2021-11-21 17:08:15 -0500

    Frontend Analizador Sintáctico

--- src/co/edu/uniquindio/compiladores/proyecto/sintaxis/Funcion.kt
+++ src/co/edu/uniquindio/compiladores/proyecto/sintaxis/Funcion.kt
@@ -1,8 +1,12 @@
 package co.edu.uniquindio.compiladores.proyecto.sintaxis
 
+import javafx.scene.control.TreeItem
+
 class Funcion(var identificador: String, var listaParametros : ArrayList<Parametro>?, var tipoRetorno: String, var listaSentencias: ArrayList<Sentencia> )
 {
     override fun toString(): String {
         return "Funcion(identificador='$identificador', listaParametros=$listaParametros, tipoRetorno='$tipoRetorno', listaSentencias=$listaSentencias)"
     }
+
+
 }
--- src/co/edu/uniquindio/compiladores/proyecto/sintaxis/UnidadDeCompilacion.kt
+++ src/co/edu/uniquindio/compiladores/proyecto/sintaxis/UnidadDeCompilacion.kt
@@ -1,7 +1,19 @@
 package co.edu.uniquindio.compiladores.proyecto.sintaxis
 
+import javafx.scene.control.TreeItem
+
 class UnidadDeCompilacion(var listaElementos: ArrayList<Elemento>) {
     override fun toString(): String {
         return "UnidadDeCompilacion(listaElementos=$listaElementos)"
+    }
+
+    fun getArbolVisual():TreeItem<String>{
+
+        var raiz = TreeItem("Unidad de Compilación")
+
+        for (f in listaElementos){
+            raiz.children.add(f.getArbolVisual())
+        }
+        return raiz
     }
 }
